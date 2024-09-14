a3c121f2d7138de55bc3dbe72e8caf4f9bdebdde
EssPi <dev.shahryar@gmail.com>
2024-09-14 19:00:09 +0330

    refactor: Enhance JSON file search to handle POST and GET-specific files

--- json-finder.go
+++ json-finder.go
@@ -7,25 +7,42 @@ import (
 	"strings"
 )
 
-var jsonAddress []string
+type JSON struct {
+	POST []string
+	GET  []string
+}
+
+var (
+	jsonAddress   JSON
+	folderAddress []string
+)
 
 func walkFunc(path string, info fs.FileInfo, err error) error {
 	if err != nil {
 		return err
 	}
-	if !info.IsDir() && strings.HasSuffix(info.Name(), ".json") {
-		jsonAddress = append(jsonAddress, path)
+	if info.IsDir() {
+		folderAddress = append(folderAddress, path)
+	}
+	if !info.IsDir() {
+		if strings.HasSuffix(info.Name(), "post.json") {
+			jsonAddress.POST = append(jsonAddress.POST, path)
+		}
+		if strings.HasSuffix(info.Name(), "get.json") {
+			jsonAddress.GET = append(jsonAddress.GET, path)
+
+		}
 	}
 	return nil
 }
 
-func GetJson() ([]string, error) {
+func Walker() (JSON, []string, error) {
 	err := filepath.Walk(Root, walkFunc)
 	if err != nil {
 		panic(err)
 	}
-	if len(jsonAddress) == 0 {
-		return nil, errors.New("can not find any json")
+	if len(jsonAddress.POST) == 0 && len(jsonAddress.GET) == 0 {
+		return JSON{}, nil, errors.New("can not find any json")
 	}
-	return jsonAddress, nil
+	return jsonAddress, folderAddress, nil
 }
