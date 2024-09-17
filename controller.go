32c942ebff0b9d9738b300e6e9b8f00ffc2acabe
EssPi <dev.shahryar@gmail.com>
2024-09-17 19:28:58 +0330

    Refactor and enhance JSON handling in API routes
    - Introduce Data type alias for map[string]interface{}
    - Rename global variables for clarity (data -> response, add request)
    - Add jsonWriter function to handle POST requests
    - Implement proper error handling and HTTP status codes
    - Update PostRoute to write received data to file
    - Improve response format for both GET and POST routes

--- controller.go
+++ controller.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-var data []map[string]interface{}
+type Data map[string]interface{}
+
+var response []Data
+var request Data
 
 func jsonReader(urlPath string, formatRequest string) error {
 	fullPath := Root + urlPath + formatRequest
@@ -18,20 +22,54 @@ func jsonReader(urlPath string, formatRequest string) error {
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&data)
+	err = decoder.Decode(&response)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func jsonWriter(urlPath string, formatRequest string) error {
+	fullPath := Root + urlPath + formatRequest
+
+	var existingData []Data
+	existingContent, err := os.ReadFile(fullPath)
+	if err == nil {
+		if err := json.Unmarshal(existingContent, &existingData); err != nil {
+			return err
+		}
+	}
+	existingData = append(existingData, request)
+
+	updatedContent, err := json.MarshalIndent(existingData, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fullPath, updatedContent, 0644)
+}
+
 func GetRoute(ctx *gin.Context) {
-	jsonReader(ctx.Request.URL.Path, "_get.json")
-	ctx.JSON(200, data)
+	if err := jsonReader(ctx.Request.URL.Path, "_get.json"); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, response)
 }
 
 func PostRoute(ctx *gin.Context) {
-	jsonReader(ctx.Request.URL.Path, "_post.json")
-	ctx.String(200, "Hi")
+	if err := ctx.BindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": err.Error()})
+		return
+	}
+	if err := jsonWriter(ctx.Request.URL.Path, "_post.json"); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": "added",
+		"data":    request,
+	})
 
 }
